6aa9a3fa27fd9f96e79e640fdfd9bc27215fbc54
priya-gajbhiye <priya.sublimetechnocorp@gmail.com>
2025-07-21 17:49:56 +0530

    Validation  error message added

--- src/app/userprofile/login/login.component.ts
+++ src/app/userprofile/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
 import { Router } from '@angular/router'; 
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,26 +10,51 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-   username: any;
+  username: any;
   password: any;
-
+  loginError:any;
 
   constructor(private loginService: LoginService, private router: Router)  { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(): void {
+ 
+// onSubmit(form: NgForm): void {
+//   if (form.valid) {
+//     this.loginService.login(this.username, this.password).subscribe(
+//       (response) => {
+//         console.log('Login successful', response);
+//         this.router.navigate(['/profile']);
+//       },
+//       (error) => {
+//         console.error('Login error', error);
+//       }
+//     );
+//   } else {
+//     console.warn('Form is invalid. Please fill in all required fields correctly.');
+//   }
+// }
+
+
+onSubmit(form: NgForm): void {
+  if (form.valid) {
     this.loginService.login(this.username, this.password).subscribe(
       (response) => {
-        console.log('Login successful', response);
-        this.router.navigate(['/profile']); 
+        if (response && response.status) {
+          this.router.navigate(['/profile']);
+        } else {
+          this.loginError = 'Invalid email or password.';
+        }
       },
       (error) => {
         console.error('Login error', error);
-       
+        this.loginError = 'Invalid email or password.';
       }
     );
+  } else {
+    this.loginError = 'Please fill all fields correctly.';
   }
+}
 
 }
